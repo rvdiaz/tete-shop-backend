0af38a669f8cf11ebd153102d04f125a7e49ccd4
rvdiaz <valdesdiazroberto@gmail.com>
2023-06-23 23:29:46 -0400

    building

--- build/transfer-tokens-edit-page.f6088097.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkshop_tete=self.webpackChunkshop_tete||[]).push([[2492],{9452:(a,t,e)=>{e.r(t),e.d(t,{default:()=>_});var s=e(67294),n=e(5458),E=e(87751),r=e(53219);const _=()=>s.createElement(n.O4,{permissions:E.Z.settings["transfer-tokens"].read},s.createElement(r.Z,null))}}]);
