66452f945ff04c7304e4dca9c9663b7cf2cd8454
rvdiaz <valdesdiazroberto@gmail.com>
2023-06-29 11:11:13 -0400

    adding features

---
+++ src/api/categories-slider/controllers/categories-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categories-slider controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::categories-slider.categories-slider');
---
+++ src/api/categories-slider/routes/categories-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categories-slider router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::categories-slider.categories-slider');
